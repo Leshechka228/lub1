94a0c982a27632a15d081debde820ea4a72077f6
Leshechka123 <alexey_2004@inbox.ru>
2023-10-31 02:28:16 +0400

    text and name

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import os
 from bs4 import BeautifulSoup
 import requests
 
-def parse_reviews():
-url = "https://www.kinopoisk.ru/film/251733/reviews/ord/date/status/all/perpage/10/page/1/"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
-reviews = soup.find_all("div", class_="response")
+# url = "https://www.kinopoisk.ru/film/251733/reviews/ord/date/status/all/perpage/10/page/1/"
+# headers = {
+#     "Accept": "*/*",
+#     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.888 YaBrowser/23.9.0.0 Safari/537.36"
+# }
+# req = requests.get(url, headers = headers)
+# src = req.text
 
-for review in reviews:
-text = review.find("span", itemprop="description").text.strip()
-rating = review.find("span", class_="rating").text.strip()
-print(f"Рецензия: {text}")
-print(f"Рейтинг: {rating}")
-print("---")
+with open("index.html", encoding="utf-8") as file:
+    src = file.read()
+soup = BeautifulSoup(src, "lxml")
+all_names = soup.find_all(class_ = "profile_name")
+all_reviews_text = soup.find_all("span", class_= "_reachbanner_")
+for name in all_names:
+    name_text = name.text
+    print(name_text)
+    print("-------")
+    for review in all_reviews_text:
+        review_text = review.text
+        print(review_text.strip())
