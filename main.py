f033c2ee597d783248a284e27e60f32d26ca7eb9
Leshechka123 <alexey_2004@inbox.ru>
2023-11-28 00:53:02 +0400

    dataset, path and "if mame == main"

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+from pathlib import Path
 
-path = r"C:\Users\User\Desktop"
+desktop_path = Path.home() / "Desktop"
 projectname = "dataset"
 folders = \
     ["good", 
     "bad"]
-fullPath = os.path.join(path, projectname)
+fullPath = os.path.join(desktop_path, projectname)
 if not os.path.exists(fullPath):
     os.mkdir(fullPath)
 for f in folders:
@@ -27,40 +28,37 @@ def parse_reviews(change):
         review_texts.append(review_text)
     return review_texts
 
-
 def write_reviews(reviews, change, count):
     for review_text in reviews:
         if count < 1000:
-            with open(fr"C:\Users\User\Desktop\dataset\{change}\{change}_{count:04}.txt", "w", encoding="utf-8") as file:
+            with open(fr"{desktop_path}\dataset\{change}\{change}_{count:04}.txt", "w", encoding="utf-8") as file:
                 file.write(review_text)
             count += 1
         else:
             break
     return count
 
+if __name__ == "__main__":
+    bad_count = 0
+    good_count = 0
+    for i in range(1, 2300):
+        url = f"https://www.kinopoisk.ru/film/251733/reviews/ord/date/status/all/perpage/10/page/{i}/"
+        options = webdriver.ChromeOptions()
+        file_path = os.path.abspath("yandexdriver.exe")
+        service = webdriver.ChromeService(executable_path=file_path)
+        driver = webdriver.Chrome(service=service)
+        driver.get(url)
+        time.sleep(10)
+        src = driver.page_source
+        soup = BeautifulSoup(src, "lxml")
+        driver.quit()
+        
+        bad_reviews = parse_reviews("bad")
+        bad_count = write_reviews(bad_reviews, "bad", bad_count)
 
-good_count = 0
-bad_count = 0
-for i in range (1, 2300):
-    url = f"https://www.kinopoisk.ru/film/251733/reviews/ord/date/status/all/perpage/10/page/{i}/"
-    headers = {
-        "Accept": "*/*",
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.888 YaBrowser/23.9.0.0 Safari/537.36"
-    }
-    src = requests.get(url, headers = headers).text
-    options = webdriver.ChromeOptions()
-    service = webdriver.ChromeService(executable_path = 'C:\\Users\\User\\Desktop\\lub1\\driver\\yandexdriver.exe')
-    driver = webdriver.Chrome(service=service)
-    driver.get(url)
-    src = driver.page_source
-    soup = BeautifulSoup(src, "lxml")
-    time.sleep(3)
-    driver.quit()
+        good_reviews = parse_reviews("good")
+        good_count = write_reviews(good_reviews, "good", good_count)
 
-    bad_reviews = parse_reviews("bad")
-    bad_count = write_reviews(bad_reviews, "bad", bad_count)
-    print(f"Плохих отзывов: {bad_count}")
-    good_reviews = parse_reviews("good")
-    good_count = write_reviews(good_reviews, "good", good_count)
-    print(f"Хороших отзывов: {good_count}")
-    print(f"\n Количество итераций: {i} \n" )
+        print(f"Плохих отзывов: {bad_count}")
+        print(f"Хороших отзывов: {good_count}")
+        print(f"\n Количество итераций: {i} \n")
